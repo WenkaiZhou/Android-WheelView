0c790b1524895edcf65bd459e3d1318877b3c9cb
zhouwenkai <zhouwenkai@baidu.com>
2019-11-16 19:23:15 +0800

    Add iOS clock demo

--- sample/src/main/java/com/kevin/wheelview/sample/MainActivity.kt
+++ sample/src/main/java/com/kevin/wheelview/sample/MainActivity.kt
@@ -15,30 +15,23 @@
  */
 package com.kevin.wheelview.sample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.kevin.wheelview.WheelView
-import com.kevin.wheelview.sample.province.ProvinceSelectorDialog
-import kotlinx.android.synthetic.main.activity_main.*
+import android.view.View
+import com.kevin.wheelview.sample.clock.IOSClockActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-
-        val list = mutableListOf(1, 212, 33, 4231435, 53, 6431, 37)
-        wheel_view.setDataItems(list)
-        wheel_view.setCyclic(true)
-        wheel_view.setTextAlign(WheelView.TEXT_ALIGN_LEFT)
-        wheel_view.setCurvedRefractRatio(1.0f)
-        wheel_view.setSoundEffect(true)
-
-        ProvinceSelectorDialog.getInstance().show(this)
-
-
-
-
+    /**
+     * iOS系统闹钟时间选择
+     */
+    fun onSystemClockClick(view: View) {
+        startActivity(Intent(this, IOSClockActivity::class.java))
     }
 }
---
+++ sample/src/main/java/com/kevin/wheelview/sample/clock/IOSClockActivity.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2019 Kevin zhou
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.kevin.wheelview.sample.clock
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kevin.wheelview.WheelView
+import com.kevin.wheelview.sample.R
+import kotlinx.android.synthetic.main.activity_ios_clock.*
+import java.util.*
+
+/**
+ * IOSClockActivity
+ *
+ * @author zhouwenkai@baidu.com, Created on 2019-11-16 13:12:29
+ *         Major Function：<b>IOS系统闹钟</b>
+ *         <p/>
+ *         Note: If you modify this class please fill in the following content as a record.
+ * @author mender，Modified Date Modify Content:
+ */
+class IOSClockActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ios_clock)
+
+
+        val hour = Calendar.getInstance()[Calendar.HOUR_OF_DAY]
+        val minute = Calendar.getInstance()[Calendar.MINUTE]
+
+        // 是否上午
+        var forenoon = hour / 12 == 0
+
+        noon_wheel_view.setDataItems(mutableListOf("上午", "下午"))
+        noon_wheel_view.setSelectedItemPosition(if (forenoon) 0 else 1)
+        hour_wheel_view.setDataItems((1..12).toMutableList())
+        hour_wheel_view.setSelectedItemPosition((hour % 12) - 1)
+        minute_wheel_view.setDataItems((1 until 60).toMutableList())
+        minute_wheel_view.setSelectedItemPosition(minute - 1)
+
+
+        var lastSelectedHour = hour
+
+        // 上下午滚轮选择监听
+        noon_wheel_view.setOnItemSelectedListener(object : WheelView.OnItemSelectedListener {
+            override fun onItemSelected(wheelView: WheelView, data: Any, position: Int) {
+                forenoon = !forenoon
+            }
+        })
+
+        hour_wheel_view.setOnItemSelectedListener(object : WheelView.OnItemSelectedListener {
+            override fun onItemSelected(wheelView: WheelView, data: Any, position: Int) {
+                val selectedHour = data as Int
+                if (lastSelectedHour == 12 && selectedHour == 1) {
+                    forenoon = !forenoon
+                    noon_wheel_view.setSelectedItemPosition(if (forenoon) 0 else 1, true)
+                } else if (lastSelectedHour == 1 && selectedHour == 12) {
+                    forenoon = !forenoon
+                    noon_wheel_view.setSelectedItemPosition(if (forenoon) 0 else 1, true)
+                }
+                lastSelectedHour = selectedHour
+            }
+        })
+    }
+}
